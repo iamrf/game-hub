06d286fa657b431166fb64373bec910fe4f3a7cc
iamrf <rf.fa17@gmail.com>
2023-12-06 15:23:23 +0330

    Displaying CriticScore

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
+import CriticScore from './CriticScore'
 
 interface Props {
     game: Game
@@ -10,9 +11,12 @@ const GameCard = ({ game }: Props) => {
         <Card overflow={'hidden'}>
             <Image src={'https://img.opencritic.com/' + game.images.banner.sm}></Image>
             <CardBody>
-                <Heading fontSize={'xl'} lineHeight='8'>
-                    {game.name}
-                </Heading>
+                <HStack>
+                    <Heading fontSize={'xl'} lineHeight='8'>
+                        {game.name}
+                    </Heading>
+                    <CriticScore score={game.topCriticScore} />
+                </HStack>
             </CardBody>
         </Card>
     )
